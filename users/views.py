0184592f0a6e2129f9907632413a568b5a5a0fb7
artem <kalaficzkij@gmail.com>
2022-10-13 15:22:39 +0300

    fix bugs, ads (last version)

--- users/views.py
+++ users/views.py
@@ -30,28 +30,47 @@ class NotaryViewSet(PsqMixin, ModelViewSet):
     parser_classes = (MultiPartParser,)
 
     psq_rules = {
-        ('create', 'update', 'partial_update', 'destroy'): [
+        ('create', 'update', 'destroy'): [
             Rule([IsAdminUser]),
         ]
     }
+
+    @extend_schema(description='Permissions: IsAdminUser | IsAuthenticated.\nGet list of notaries.')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nGet notary by id.')
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nCreate new notary.')
+    def create(self, request, *args, **kwargs):
+        return super().create(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nUpdate notary by id.')
+    def update(self, request, *args, **kwargs):
+        return super().update(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nDelete notary by id.')
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, args, kwargs)
 # endregion Notaries
 
 
 # region User Profile
-@extend_schema(description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer)')
 class UserViewSet(GenericViewSet):
     queryset = UserProfile.objects.filter(is_staff=False, is_developer=False)
     serializer_class = UserProfileSerializer
     permission_classes = [IsMyProfile]
     parser_classes = (MultiPartParser,)
 
-    @extend_schema(methods=['get'])
+    @extend_schema(methods=['get'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nGet all information for current user')
     @action(detail=False)
     def my_profile(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nUpdate personal information')
     @action(detail=False, methods=['put'], serializer_class=UserProfileSerializer)
     def change_my_contacts(self, request):
         serializer = self.serializer_class(request.user, data=request.data, partial=True)
@@ -59,7 +78,7 @@ class UserViewSet(GenericViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nUpdate information for agent')
     @action(detail=False, methods=['put'], serializer_class=UserAgentContactsSerializer)
     def change_agent_contacts(self, request):
         serializer = self.serializer_class(request.user, data=request.data, partial=True)
@@ -67,13 +86,13 @@ class UserViewSet(GenericViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nRenew subscription for current user')
     @action(detail=False, methods=['put'], serializer_class=UserSubscriptionRenewalSerializer)
     def renew_subscription(self, request):
         renew_subscription(request.user)
         return Response({'status': 'Подписка успешно продлена!'})
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nOff or on autorenewal status for subscription')
     @action(detail=False, methods=['put'], serializer_class=UserSubscriptionSerializer)
     def change_auto_renewal_status(self, request):
         serializer = self.serializer_class(request.user, data=request.data, partial=True)
@@ -81,7 +100,7 @@ class UserViewSet(GenericViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nChange recipient for getting notices')
     @action(detail=False, methods=['put'], serializer_class=UserManageNoticeSerializer)
     def change_recipient_for_notices(self, request):
         serializer = self.serializer_class(request.user, data=request.data, partial=True)
@@ -89,7 +108,7 @@ class UserViewSet(GenericViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(methods=['put'])
+    @extend_schema(methods=['put'], description='Permissions: IsAuthenticated & IsMyProfile(only user, not admin or developer).\nSwitch getting calls and messages to agent')
     @action(detail=False, methods=['put'], serializer_class=UserSwitchNoticesSerializer)
     def switch_calls_to_agent(self, request):
         serializer = self.serializer_class(request.user, data=request.data, partial=True)
@@ -100,7 +119,6 @@ class UserViewSet(GenericViewSet):
 
 
 # region Chats
-@extend_schema(description='Permissions: IsAuthenticated')
 class MessageViewSet(mixins.ListModelMixin,
                      mixins.CreateModelMixin,
                      GenericViewSet):
@@ -121,15 +139,18 @@ class MessageViewSet(mixins.ListModelMixin,
     @extend_schema(
         parameters=[
             OpenApiParameter(name='recipient', description='Filter by chats', required=False, type=int)
-        ]
+        ], description='Permissions: IsAuthenticated.\nGet all messages. Get all messages of chat between current user and companion by id'
     )
     def list(self, request):
         return super().list(request)
+
+    @extend_schema(description='Permissions: IsAuthenticated.\nSend message to user by id')
+    def create(self, request, *args, **kwargs):
+        return super().create(request, args, kwargs)
 # endregion Chats
 
 
 # region Filters
-@extend_schema(description='Permissions: IsAuthenticated & IsMyFilter')
 class FilterViewSet(mixins.ListModelMixin,
                     mixins.RetrieveModelMixin,
                     mixins.CreateModelMixin,
@@ -143,6 +164,22 @@ class FilterViewSet(mixins.ListModelMixin,
     def get_queryset(self):
         queryset = Filter.objects.filter(user=self.request.user)
         return queryset
+
+    @extend_schema(description='Permissions: IsAuthenticated & IsMyFilter.\nGet all filter for user')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAuthenticated & IsMyFilter.\nGet filter by id')
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAuthenticated & IsMyFilter.\nCreate new filter. Must be unique type for new filter. There are 4 filters in total')
+    def create(self, request, *args, **kwargs):
+        return super().create(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAuthenticated & IsMyFilter.\nUpdate filter by id')
+    def update(self, request, *args, **kwargs):
+        return super().update(request, args, kwargs)
 # endregion MyFilters
 
 
@@ -159,6 +196,14 @@ class ModerationUserListApiView(mixins.ListModelMixin,
     filterset_fields = ['in_blacklist']
     search_fields = ['first_name', 'last_name']
 
+    @extend_schema(description='Permissions: IsAdminUser.\nGet all users.')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nAdd or remove user from blacklist.')
+    def update(self, request, *args, **kwargs):
+        return super().update(request, args, kwargs)
+
 
 @extend_schema(description='Permissions: IsAdminUser')
 class ModerationAdViewSet(mixins.ListModelMixin,
@@ -169,4 +214,16 @@ class ModerationAdViewSet(mixins.ListModelMixin,
     http_method_names = ["put", "get"]
     serializer_class = ModerationAdSerializer
     permission_classes = [IsAdminUser]
+
+    @extend_schema(description='Permissions: IsAdminUser.\nGet all ads for moderation.')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nGet ad by id for moderation.')
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, args, kwargs)
+
+    @extend_schema(description='Permissions: IsAdminUser.\nUpdate moderation action for ad by id.')
+    def update(self, request, *args, **kwargs):
+        return super().update(request, args, kwargs)
 # endregion Moderation
